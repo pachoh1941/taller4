075f1e70da0d2c728f1bd3f8399ddf28e30eaa76
pachoh1941 <pachoh1941@users.noreply.github.com>
2017-05-02 07:18:21 -0500

    Se inicializa y=exp(-x)

--- integracion.c
+++ integracion.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Declaracion e funciones*/
+void func(double *f, double *x, int tamano);
+/*Main*/
 int main
 {
-  
+  int i;
+  double x_min = 0.0;
+  double x_max = 1.0;
+  double y_min = 0.0;
+  int puntos = 10000;
+  double dx = (x_max-x_min)/puntos;
+  double *x;
+  x = malloc(puntos*sizeof(int));
+  double *y;
+  y = malloc(puntos*sizeof(int));
+  x[0] = x_min;
+  /*Inicializamos x*/
+  for(i=1;i<puntos;i++){
+    x[i] = x[i-1] + dx;
+  }
+  /*Inicializamos y = exp(-x)*/
+  func(y,x,puntos);
 }
-
+/*Funcion que calcula exp(-x)*/
 void func(double *f, double *x, int tamano)
 {
+  int i;
   for(i=0;i<tamano;i++) {
     f[i] = exp(-x[i]);
     }
 }
+
